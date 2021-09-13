b5c78a0d2453789d780499fb4b6269abf9566042
MatthijsBiondina <biondina.matthijs@gmail.com>
2021-09-13 14:10:04 +0200

    abstract 3d simulation

--- biobots3D/simulation/abstractsimulation3d.py
+++ biobots3D/simulation/abstractsimulation3d.py
@@ -5,4 +5,9 @@ from utils.errors import TodoException
 
 class AbstractSimulation3D(ABC):
     def __init__(self):
-        raise TodoException
+        super(AbstractSimulation3D, self).__init__()
+
+        self.next_cell_id = 0
+        self.next_surf_id = 0
+        self.next_edge_id = 0
+        self.next_node_id = 0
